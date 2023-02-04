3fd2f071e764f7505191a6a6cf02c5b927b546c5
Xingjian Xie <xingjian.xie21@imperial.ac.uk>
2023-02-04 15:19:07 +0000

    Initial Commit

---
+++ Chase.IO/Chase_IOApp.swift
@@ -0,0 +1,17 @@
+//
+//  Chase_IOApp.swift
+//  Chase.IO
+//
+//  Created by 谢行健 on 04/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct Chase_IOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
